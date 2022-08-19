127b48a4fee5d01a1752e2f76e4efbf2b7481c33
sebastianf777 <sebastianf777@outlook.com>
2022-08-18 23:20:19 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+var app = express();
+
+// Set up mongoose connection
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://m001-student:<password>@sandbox.x351nnc.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.x351nnc.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/genre.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema(
+  {
+    name: { type: String, required: true, minLength: 3, maxLength: 100 },
+  }
+);
+
+
+// Virtual for genre's URL
+GenreSchema
+  .virtual('url')
+  .get(() => {
+    return '/catalog/genre/' + this._id;
+  });
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
